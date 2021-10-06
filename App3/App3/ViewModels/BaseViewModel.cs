512c6de0757bf2c55e18356f083515205f796dd3
JORGE ALAN SALAS MONTOYA <2018371023@uteq.edu.mx>
2021-10-05 19:45:56 -0500

    Agregar archivos de proyecto.

---
+++ App3/App3/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App3.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string nombrepropiedad)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nombrepropiedad));
+        }
+    }
+}
---
+++ App3/App3/ViewModels/NombreComViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3.ViewModels
+{
+    public class NombreComViewModel : BaseViewModel
+    {
+        string nombre;
+        string ap1;
+        string ap2;
+
+        public string Nombre 
+        {
+            get => nombre;
+            set 
+            {
+                if (string.Equals(nombre, value)) return;
+                nombre = value;
+                OnPropertyChanged(nameof(Nombre));
+                OnPropertyChanged(nameof(NombreCom));
+            }
+        }
+
+        public string Ap1
+        {
+            get => nombre;
+            set
+            {
+                if (string.Equals(ap1, value)) return;
+                ap1 = value;
+                OnPropertyChanged(nameof(Ap1));
+                OnPropertyChanged(nameof(NombreCom));
+            }
+        }
+
+        public string Ap2
+        {
+            get => nombre;
+            set
+            {
+                if (string.Equals(ap2, value)) return;
+                ap1 = value;
+                OnPropertyChanged(nameof(Ap2));
+                OnPropertyChanged(nameof(NombreCom));
+            }
+        }
+
+        public string NombreCom
+        {
+            get => string.Format("Tu nombre completo es {0} {1} {2}", nombre, ap1, ap2);
+        }
+    }
+}
